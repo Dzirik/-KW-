6d5da0637ed01426e80149664dc84dbcfe6f9d3a
Jiří Palek <jiri.palek@gmail.com>
2015-08-09 11:14:15 +0200

    Network
    První fungující verze ...

--- Network/NetworkApp.R
+++ Network/NetworkApp.R
@@ -34,94 +34,50 @@ ui <- dashboardPage(
   ## Body content-----------------------------------------------------------------------------------
   dashboardBody(
     tabItems(
+      #About content
+      tabItem(tabName="About",
+        h2("tady bude text")
+      ),
       
-            #Simple Network content
-            tabItem(tabName="SimpleNetwork",
-              fluidRow(
-                box(plotOutput("plot1")),
-                
-                box(
-                  title="Nadpis",
-                  solidHeader=TRUE,
-                  collapsible=TRUE,
-                  height=300,
-                  "Box content here", 
-                  br(), 
-                  "More box content",
-                  sliderInput("slider", "Slider input:", 1, 100, 50),
-                  textInput("text", "Text input:")
-                )
-              )
-            ),
-            tabItem(tabName = "dashboard",
-                    h2("Dashboard tab content")
-            ),
-            
-            tabItem(tabName = "widgets",
-                    h2("Widgets tab content")
-            )      
+      #Force network content
+      tabItem(tabName="ForceNetwork",
+        h2("tady bude obsah")
+      ),
+      
+      #Simple Network content
+      tabItem(tabName="SimpleNetwork",
+        fluidRow(
+          box(plotOutput("plot1")
+          ),
+          
+          box(
+            title="Nadpis",
+            solidHeader=TRUE,
+            collapsible=TRUE,
+            height=300,
+            "Box content here", 
+            br(), 
+            "More box content",
+            sliderInput("slider", "Slider input:", 1, 100, 50),
+            textInput("text", "Text input:")
+          )
+        )
+      ),
+      
+      tabItem(tabName = "dashboard",
+              h2("Dashboard tab content")
+      ),
+      
+      
+      tabItem(tabName = "widgets",
+              h2("Widgets tab content")
+      )      
     )
+  )  
+)    
     
-    
-    
-#     tabItems(
-#       #About content
-#       tabItem(tabName="About",
-#               h2("tady bude text")
-#       ),
-#       
-#       #Simple Network content
-#       tabItem(tabName="SimpleNetwork",
-#         fluidRow(
-#           box(plotOutput("plot1")),
-#           
-#           box(
-#             title="Nadpis",
-#             solidHeader=TRUE,
-#             collapsible=TRUE,
-#             height=300,
-#             "Box content here", 
-#             br(), 
-#             "More box content",
-#             sliderInput("slider", "Slider input:", 1, 100, 50),
-#             textInput("text", "Text input:")
-#           )
-#         )
-#       ),
-#     
-#       #Force network content
-#       tabItem(tabName="ForceNetwork",
-#         h2("tady bude obsah")
-#       ),
-#       
-#       tabItem(tabName = "dashboard",
-#               h2("Dashboard tab content")
-#       ),
-#       
-#       tabItem(tabName = "widgets",
-#               h2("Widgets tab content")
-#       )
-#     )
-    
-    #tabItems(
-      # First tab content
-      #tabItem(tabName = "Simple Network",
-      #        fluidRow(
-#                 box(plotOutput("plot1", height = 250)),
-#                 
-#                 box(
-#                   title = "Controls",
-#                   sliderInput("slider", "Number of observations:", 1, 100, 50)
-#                 )
-#               )
-#       ),
-      
-      #
-      #tabItem(tabName= "K-means"
-      #),
-#     )
-#   )
-)
+### SERVER -----------------------------------------------------------------------------------------
+### SERVER -----------------------------------------------------------------------------------------
 
 server <- function(input, output) {
   set.seed(122)
